Corrected 1D spectra calculation, updated gold, added dissipation.

tests/test_turbulence_postprocessing.py
@@ -29,21 +29,21 @@ class TurbulencePostProcessingTestCase(unittest.TestCase):
         spectra = tp.energy_spectra([self.U, self.V, self.W], self.N, self.L)
 
         npt.assert_array_almost_equal(spectra[spectra['name'] == 'E00(k0)'].E,
-                                      np.array([2.4385440189650232e+00,
-                                                2.9914191778951629e-03,
-                                                2.0322422333186274e-01,
-                                                5.0888977717213207e-03,
-                                                1.9856548956683193e-01,
-                                                5.3322259537962715e-03,
-                                                4.0160669234318592e-03,
-                                                9.6830518347628923e-03,
-                                                1.9548206246123070e-01,
-                                                2.0993390517673238e-02,
-                                                3.1417705304799411e-03,
-                                                1.0810948863068269e-03,
-                                                4.9213284563453706e-04,
-                                                2.3880265240150747e-04,
-                                                1.1354528541439582e-04]),
+                                      np.array([1.5524253382618400e+00,
+                                                1.9043965960876359e-03,
+                                                1.2937655879710899e-01,
+                                                3.2396929410351194e-03,
+                                                1.2641071676809387e-01,
+                                                3.3946004729182920e-03,
+                                                2.5567076106081631e-03,
+                                                6.1644222548702448e-03,
+                                                1.2444774610601403e-01,
+                                                1.3364807492584877e-02,
+                                                2.0001132399453158e-03,
+                                                6.8824638042840861e-04,
+                                                3.1330150016246905e-04,
+                                                1.5202649021262232e-04,
+                                                7.2285173753924731e-05]),
                                       decimal=10)
 
     def test_energy_spectra_3D(self):
@@ -149,6 +149,13 @@ class TurbulencePostProcessingTestCase(unittest.TestCase):
                                                 0.0968242917758608,
                                                 0.0198170012870652]),
                                       decimal=10)
+
+    def test_dissipation(self):
+        """Is the dissipation calculation correct?"""
+        dissipation = tp.dissipation(
+            [self.U, self.V, self.W], self.N, self.L, 0.4)
+
+        npt.assert_almost_equal(dissipation, 51.7389774006043481)
 
 
 if __name__ == '__main__':

turbulence_postprocessing/turbulence_postprocessing.py
@@ -18,7 +18,8 @@ import scipy.integrate as spi
 #
 # ========================================================================
 def energy_spectra(U, N, L):
-    """Get the 1D and 3D energy spectra from 3D data.
+    """
+    Get the 1D and 3D energy spectra from 3D data.
 
     The 3D energy spectrum is defined as (see Eq. 6.188 in Pope):
 
@@ -43,7 +44,6 @@ def energy_spectra(U, N, L):
     :type L: list
     :return: Dataframe of 1D and 3D energy spectra
     :rtype: dataframe
-
     """
 
     # =========================================================================
@@ -99,7 +99,7 @@ def energy_spectra(U, N, L):
         E = np.zeros(len(kbins) - 2)
         for k, n in enumerate(range(1, len(kbins) - 1)):
             area = np.pi * kmax[jm] * kmax[jn] * np.sqrt(1 - (k / kmax[j])**2)
-            E[k] = 0.5 * np.pi * np.mean(Ef.flat[whichbin == n]) * area
+            E[k] = np.mean(Ef.flat[whichbin == n]) * area
         E[E < 1e-13] = 0.0
 
         # Store the data
@@ -135,6 +135,49 @@ def energy_spectra(U, N, L):
     df = pd.concat([df, subdf], ignore_index=True)
 
     return df
+
+
+# ========================================================================
+def dissipation(U, N, L, viscosity):
+    """
+    Get the dissipation.
+
+    The dissipation is defined as (see Eq. 6.160 in Pope):
+
+    - :math:`\\epsilon = 2 \\nu \\sum_k k^2 E({\\mathbf{k}})`
+
+    where :math:`k=\\sqrt{k_0^2 + k_1^2 + k_2^2}`.
+
+    :param U: momentum, [:math:`u`, :math:`v`, :math:`w`]
+    :type U: list
+    :param N: number of points, [:math:`n_x`, :math:`n_y`, :math:`n_z`]
+    :type N: list
+    :param L: domain lengths, [:math:`L_x`, :math:`L_y`, :math:`L_z`]
+    :type L: list
+    :param viscosity: kinematic viscosity
+    :type viscosity: double
+    :return: dissipation
+    :rtype: double
+    """
+
+    # FFT of fields
+    Uf = [np.fft.fftn(U[0]),
+          np.fft.fftn(U[1]),
+          np.fft.fftn(U[2])]
+
+    # Wavenumbers
+    k0 = np.fft.fftfreq(Uf[0].shape[0]) * N[0]
+    k1 = np.fft.fftfreq(Uf[0].shape[1]) * N[1]
+    k2 = np.fft.fftfreq(Uf[0].shape[2]) * N[2]
+    K = np.meshgrid(k0, k1, k2)
+    kmag2 = K[0]**2 + K[1]**2 + K[2]**2
+
+    # Energy in Fourier space
+    Ef = 0.5 / (np.prod(N)**2) * (np.absolute(Uf[0])**2
+                                  + np.absolute(Uf[1])**2
+                                  + np.absolute(Uf[2])**2)
+
+    return 2.0 * viscosity * np.sum(kmag2 * Ef)
 
 
 # ========================================================================